12ee497136328854597edd82856e2216ce6b7376
Ivan2 <moscosoi2002@gmail.com>
2024-07-08 14:02:19 +0200

    fotos de cada equipo en equios

--- src/components/Equipo.jsx
+++ src/components/Equipo.jsx
@@ -6,33 +6,44 @@ import { Link } from 'react-router-dom';
 import EstadisticaController from '../controllers/EstadisticasController';
 import CardJugador from './CardJugador';
 import JugadorController from '../controllers/JugadoresController';
+import EquipoController from '../controllers/EquipoController';
 
 
 
 function Equipo() {
   let { id } = useParams();
   const jugadorcontroller = new JugadorController();
-  const estadisticacontroller = new EstadisticaController();
+  const equipocontroller = new EquipoController();
+  
+  const [equipo, setEquipo] = useState(null);
+  const [logo , setLogo] = useState(null);
 
   const [jugadores, setJugadores] = useState([]);
   const [count, setCount] = useState(0);
 
   useEffect(() => {
+    const getEquipos = async () => {
+      const equiposData = await equipocontroller.getEquipoById(id);
+      setEquipo(equiposData);
+      setLogo(equiposData.Logo)
+    };
     const fetchJugadores = async () => {
       const jugadoresData = await jugadorcontroller.getJugadoresByEquipo(id);
       setJugadores(jugadoresData);
       console.log(jugadoresData)
     };
-
+    getEquipos();
     fetchJugadores();
   }, [id]);
 
 
 
 
+
   return (
     <div className="equipo-container">
-      <header className="equipo-header">
+  <header className='equipo-header'>
+    <img className='banner' src={logo} alt="" />
 
       </header>
       <h1 className="equipo-nombre">Nombre del Equipo</h1>
